e92b96206c8f81e4cad0838b59ef5c6fd4ec9ae4
Bojan Božić <boj.bozic@gmail.com>
2018-03-28 23:27:08 +0100

    improved formatting to get rid of unnecessary spaces. who needs them at all? am i right? am i right???

--- multi-label-classifier.py
+++ multi-label-classifier.py
@@ -51,49 +51,52 @@ def multi_label_classification(data,books=True,test_size=.2):
     
     # Add Books
     if books:
-        train_x_books = train['book_name'].apply(lambda x: x[0]).as_matrix()
-        test_x_books = test['book_name'].apply(lambda x: x[0]).as_matrix()
-        count_vectorizer_books = CountVectorizer(stop_words="english", tokenizer=tokenize, ngram_range=(1, 3), max_features=10000, analyzer="word")
-        cv_train_x_books = count_vectorizer_books.fit_transform(train_x_books)
-        cv_test_x_books = count_vectorizer_books.transform(test_x_books)
-        tfidf_vectorizer_books = TfidfVectorizer(stop_words="english", tokenizer=tokenize, ngram_range=(1, 3),
-                                   max_features=10000, analyzer="word")
-        tv_train_x_books = tfidf_vectorizer_books.fit_transform(train_x_books)
-        tv_test_x_books = tfidf_vectorizer_books.transform(test_x_books)
-        cv_train_x = hstack([cv_train_x_books,cv_train_x_content])
-        cv_test_x = hstack([cv_test_x_books,cv_test_x_content])
-        tv_test_x = hstack([tv_test_x_books,tv_test_x_content])
-        tv_train_x = hstack([tv_train_x_books,tv_train_x_content])
+        train_x_books=train['book_name'].apply(lambda x:x[0]).as_matrix()
+        test_x_books=test['book_name'].apply(lambda x:x[0]).as_matrix()
+        count_vectorizer_books=CountVectorizer(stop_words="english",tokenizer=tokenize,
+                                               ngram_range=(1,3),max_features=10000,
+                                               analyzer="word")
+        cv_train_x_books=count_vectorizer_books.fit_transform(train_x_books)
+        cv_test_x_books=count_vectorizer_books.transform(test_x_books)
+        tfidf_vectorizer_books=TfidfVectorizer(stop_words="english",tokenizer=tokenize,
+                                               ngram_range=(1,3),max_features=10000,
+                                               analyzer="word")
+        tv_train_x_books=tfidf_vectorizer_books.fit_transform(train_x_books)
+        tv_test_x_books=tfidf_vectorizer_books.transform(test_x_books)
+        cv_train_x=hstack([cv_train_x_books,cv_train_x_content])
+        cv_test_x=hstack([cv_test_x_books,cv_test_x_content])
+        tv_test_x=hstack([tv_test_x_books,tv_test_x_content])
+        tv_train_x=hstack([tv_train_x_books,tv_train_x_content])
     else:
-        cv_train_x = cv_train_x_content 
-        cv_test_x = cv_test_x_content
-        tv_train_x = tv_train_x_content
-        tv_test_x = tv_test_x_content
+        cv_train_x,cv_test_x,tv_train_x,tv_test_x=cv_train_x_content,cv_test_x_content,
+        tv_train_x_content,tv_test_x_content
     
     # Generate predictions using counts
-    classifier.fit(cv_train_x, train_labels)
-    filename1 = 'count_vectorizer_model.pkl'
-    pickle.dump(classifier, open(filename1, 'wb'))
+    classifier.fit(cv_train_x,train_labels)
+    filename1='count_vectorizer_model.pkl'
+    pickle.dump(classifier,open(filename1,'wb'))
     
     # Generate predictions using tf-idf representation
-    classifier.fit(tv_train_x, train_labels)
-    filename2 = 'tfidf_vectorizer_model.pkl'
-    pickle.dump(classifier, open(filename2, 'wb'))
+    classifier.fit(tv_train_x,train_labels)
+    filename2='tfidf_vectorizer_model.pkl'
+    pickle.dump(classifier,open(filename2,'wb'))
     
     # Import trained models
-    count_vectorizer_model, tfidf_vectorizer_model = joblib.load(filename1), joblib.load(filename2)
+    count_vectorizer_model,tfidf_vectorizer_model=joblib.load(filename1),
+    joblib.load(filename2)
     
-    # Making predictions
-    cv_pred = count_vectorizer_model.predict(cv_test_x.toarray())
-    tv_pred = tfidf_vectorizer_model.predict(tv_test_x.toarray())
+    # Prediction assignment
+    cv_pred,tv_pred=count_vectorizer_model.predict(cv_test_x.toarray()),
+    tfidf_vectorizer_model.predict(tv_test_x.toarray())
     
-    # Combine predictions and map the labels if the values do not equal 0, else assign empty string
-    combined_pred = np.where((cv_pred + tv_pred) != 0, mlb.classes_, "")
+    # Combine predictions and map labels
+    combined_pred=np.where((cv_pred+tv_pred)!=0,mlb.classes_,"")
     
     # Load the array into a DataFrame constructor and join non-empty strings
-    predictions = pd.DataFrame(combined_pred).apply(join_strings, axis=1).to_frame("predicted")
-    predictions['predicted'] = predictions['predicted'].apply(lambda x: x.split())
-    predictions=pd.concat([test['tags'].reset_index(),predictions['predicted']],axis=1,keys=['original','predicted'],ignore_index=True)
+    predictions=pd.DataFrame(combined_pred).apply(join_strings,axis=1).to_frame("predicted")
+    predictions['predicted']=predictions['predicted'].apply(lambda x:x.split())
+    predictions=pd.concat([test['tags'].reset_index(),predictions['predicted']],axis=1,
+                          keys=['original','predicted'],ignore_index=True)
     predictions.drop(0,axis=1,inplace=True)
     predictions.columns=['original','predicted']
     return predictions
