b86108f60283f9b4945d2b495bfcc60f581a025c
Bojan Božić <boj.bozic@gmail.com>
2018-04-07 00:42:37 +0100

    added book names as feature

--- knn.py
+++ knn.py
@@ -1,32 +1,64 @@
-def knn(data,k):
-    texts=list(df['content'])
-    predictions=[]
-    for text in texts:
-        dist_cos={}
-        lcs=[]
-        df_x_cn = df['content'].as_matrix()
-        tfidf_vectorizer_cn = TfidfVectorizer(stop_words="english", tokenizer=tokenize, ngram_range=(1, 3),max_features=10000, analyzer="word")
-        tfidf_df_x_cn = tfidf_vectorizer_cn.fit_transform(df_x_cn)
-        ltag=df.tags.tolist()
-        vec_cn=tfidf_vectorizer_cn.transform([clean(text)])
-        vec=vec_cn
-        for i in range(df.shape[0]):
-            dist_cos[i]={'tags':ltag[i],'cs':cosine_similarity(vec,tfidf_df_x_cn[i:i+1])[0,0]}
-            lcs.append(cosine_similarity(vec,tfidf_df_x_cn[i:i+1])[0,0])
-        r=np.array(lcs)
-        rt=[ltag[j] for j in (r.argsort()[-k:][::-1]).tolist()]
-        if k>1:
-            a1=set(rt[1])
-            for i in range(1,k):
-                a2=a1&set(rt[i])
-                a1=a2
-            res=set(rt[0]) | a1
-        else:
-            res=set(rt[0])
-        predictions.append(res)
-    return predictions
+import numpy as np
+import pandas as pd
 
-start=time.time()
-print(knn(df,2))
-end=time.time()
-display(end-start)
+from scipy.sparse import hstack
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk import word_tokenize
+from tagging.helper import clean
+
+def knn(data,k,include_books=True):
+    texts=list(data['content'])
+    tags=data.tags.tolist()
+    predictions=[]
+    vectorizer=TfidfVectorizer(stop_words='english',tokenizer=word_tokenize,
+                               ngram_range=(1,3),max_features=10000,analyzer='word')
+    X=vectorizer.fit_transform(data['content'].as_matrix())
+    if include_books:
+        books=list(data['book_name'].apply(lambda x:x[0]).as_matrix())
+        vectorizer_b=TfidfVectorizer(stop_words='english',tokenizer=word_tokenize,
+                                      ngram_range=(1,3),max_features=10000,analyzer='word')
+        X_b=vectorizer_b.fit_transform(data['book_name'].apply(lambda x:x[0]).as_matrix())
+        X=hstack([X_b,X]).toarray()
+        for text,books in zip(texts,books):
+            dist_cos={}
+            lcs=[]
+            vec_t=vectorizer.transform([clean(text)])
+            vec_b=vectorizer_b.transform([clean(books)])
+            vec=hstack([vec_b,vec_t]).toarray()
+            for i in range(data.shape[0]):
+                dist_cos[i]={'tags':tags[i],'cs':cosine_similarity(vec,X[i:i+1])[0,0]}
+                lcs.append(cosine_similarity(vec,X[i:i+1])[0,0])
+            r=np.array(lcs)
+            rt=[tags[j] for j in (r.argsort()[-k:][::-1]).tolist()]
+            if k>1:
+                a1=set(rt[1])
+                for i in range(1,k):
+                    a2=a1&set(rt[i])
+                    a1=a2
+                res=set(rt[0]) | a1
+            else:
+                res=set(rt[0])
+            predictions.append(list(res))
+    else:
+        for text in texts:
+            dist_cos={}
+            lcs=[]
+            vec=vectorizer.transform([clean(text)])
+            for i in range(data.shape[0]):
+                dist_cos[i]={'tags':tags[i],'cs':cosine_similarity(vec,X[i:i+1])[0,0]}
+                lcs.append(cosine_similarity(vec,X[i:i+1])[0,0])
+            r=np.array(lcs)
+            rt=[tags[j] for j in (r.argsort()[-k:][::-1]).tolist()]
+            if k>1:
+                a1=set(rt[1])
+                for i in range(1,k):
+                    a2=a1&set(rt[i])
+                    a1=a2
+                res=set(rt[0]) | a1
+            else:
+                res=set(rt[0])
+            predictions.append(list(res))
+    predictions=pd.DataFrame({'predictions':predictions})
+    predictions['original']=data.tags
+    return predictions
